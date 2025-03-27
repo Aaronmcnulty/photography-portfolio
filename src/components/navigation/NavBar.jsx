b21a3d92a238a52b9a7fd743f290b15ae6547d0a
malcomcrab <malcomcrabesq@outlook.com>
2025-03-27 15:07:53 +0000

    Style: Adds keyframe animation to NavBar links when visibility is toggled

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,16 +1,36 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./navBar.module.css"
 
 function NavBar(){
 
+    const [isHidden, setIsHidden] = useState(true)
+
+    const handleMenuClick = () => {
+        if(isHidden == true){
+            setIsHidden(false)
+        } else {
+            setIsHidden(true)
+        }
+        console.log(isHidden)
+    }
+
     return(
-        <div className={styles.navBarContainer}>
-            <Link className={styles.navLink} to="/">Home</Link>
-            <Link className={styles.navLink} to="gallery">Gallery</Link>
-            <Link className={styles.navLink} to="contact">Contact</Link>
-        </div>
-        
+            <div className={styles.navBarContainer}>
+
+               
+                <div className={styles.menuToggle}>
+                    <p onClick={handleMenuClick} >///</p>
+                </div>
+                {isHidden &&  
+                    <div className={styles.navLinksContainer}>
+                        <Link className={styles.navLink} to="/">Home</Link>
+                        <Link className={styles.navLink} to="gallery">Gallery</Link>
+                        <Link className={styles.navLink} to="contact">Contact</Link>
+                    </div>
+                }
+            </div>
+
     )
 
 }
