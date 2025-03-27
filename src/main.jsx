54c3b51508fb5d6ce747529dbcf07b2b0dae7686
malcomcrab <malcomcrabesq@outlook.com>
2025-03-27 11:01:40 +0000

    Feat: Adds Error page and navigation for incorrect urls

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import App from './App.jsx'
 import ContactPage from './components/contact/ContactPage.jsx'
 import HomePage from './components/homepage/HomePage.jsx'
+import ErrorPage from './components/errorComponents/UrlError.jsx'
 
 const router = createBrowserRouter([
 
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "contact", element: <ContactPage /> },
